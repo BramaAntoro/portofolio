7c66941876b00e73d41686b26dae5dfd5c2d5a90
BramaAntoro <brama4751antoro@gmail.com>
2024-03-06 12:14:11 +0700

    update portofolio

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
-// function SendEmail(){
-//     var params = {
-//         from_name : document.getElementById("fullName").value,
-//         email_id : document.getElementById("email_id").value,
-//         message : document.getElementById("message").value
-//     }
-//     emailjs.send("service_9zqjyri", "template_ah9t8n5", params).thenz(function res(){
-//         alert("Success! " + res.status);
-//     })
-// }
+function SendMail(){
+    var params = {
+        from_name : document.getElementById("forInput").value,
+        email_id : document.getElementById("emailInput").value,
+        message : document.getElementById("textareaInput").value
+    }
+    emailjs.send("service_9zqjyri", "template_ah9t8n5", params).then(function (res) {
+        alert("Succes! ");
+    })
+}
